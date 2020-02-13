6455982dff1f29ea40d8248f6e4b08a7cce71daf
Kris <christie.pautova@gmail.com>
2020-02-13 18:09:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+
+let persone = {
+    name : 'jone',
+    age: 25,
+    isMarried: false
+};
+console.log(persone['name'])
+
+let arr = ["plum.png", "organge.jpg", 'apple.bmp'];
+
+console.log(arr[2]);
+
+//alert('Hello world');
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?", 'Да');
+
+//console.log(typeof(answer));
+
+//console.log('arr' + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+ 
+
+ console.log(incr++);
+ console.log(decr--);
+
+ console.log(5%2)
+ console.log('2'=== 2);
+
+ let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
