4d581eb4fc5af19ae40719210b412941caa91a2c
Kris <christie.pautova@gmail.com>
2020-03-05 21:54:21 +0600

    homework11

---
+++ input.js
@@ -0,0 +1,8 @@
+//<input id="age" value="30">
+
+let age = document.getElementById('age');
+
+age.addEventListener('click', ) function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+showUser();
